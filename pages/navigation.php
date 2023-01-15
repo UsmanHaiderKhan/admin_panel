283739ad47eab7a9dcfb8a5df6384ea7038ced97
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-01-15 12:59:48 +0500

    🎉 Add : Navigation bar

---
+++ pages/navigation.php
@@ -0,0 +1,31 @@
+
+     <div class="navigation">
+        <div class="n1">
+        <div>
+          <i id="menu-btn" class="fa fa-bars"></i>
+        </div>
+        <div class="search">
+          <i class="fa fa-search"></i> &nbsp;
+          <input type="text" placeholder="Search" />
+        </div>
+        </div>
+        <div class="profile">
+          <div>
+            <i class="far fa-bell"></i> &nbsp;&nbsp;
+            <img src="assets/img/team/team-1.jpg" alt="" />
+         </div>
+         &nbsp;
+           <div class="dropdown">
+          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+           Awaise Chohan
+          </a>
+
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <li><a class="dropdown-item" href="#"><i class="fa fa-user"></i>&nbsp; User Detail</a></li>
+            <li><a class="dropdown-item" href="#"><i class="fa fa-sign-out-alt"></i>&nbsp; Logout</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+          </div>
+         </div>
+     </div>
+  
