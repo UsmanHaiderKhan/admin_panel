2ea599d3512e91a294d7c49cf7f84c34ca1b4f85
Ayaz Ali <122862438+AyazSulahri@users.noreply.github.com>
2023-02-22 13:04:57 +0500

    ♻ update : the files locations

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// /*************search bar************* */
+
+// const searchFun = () => {
+//     let filter = document.getElementById('myInput').ariaValueMax.toUpperCase();
+//     let myTable = document.getElementById('myTable');
+//     let tr = myTable.getElementsByTagName('tr');
+//     for()
+// }
